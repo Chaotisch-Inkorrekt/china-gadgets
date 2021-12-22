801389e78c242b839575ec7ded86db6a28f7c25e
Fenrikur <github@fenrikur.de>
2021-12-22 14:58:50 +0100

    add gadget html generation code

--- bin/generate-html.pl
+++ bin/generate-html.pl
@@ -16,6 +16,8 @@ my $htmlTemplateFile    = "$htmlOutputDirectory/gadget-000.tpl.html";
 
 my $gadgetIdPattern = '%03s%s';
 my $htmlOutputPathPattern = "$htmlOutputDirectory/gadget-%s.html";
+my $imageSourcePattern = '<li><a href="{{url}}">{{title}}</a></li>';
+my $linkPattern = '<li><a href="{{url}}">{{title}}</a></li>';
 my $readmeGadgetPattern = "| [{{gadget}}]({{htmlOutputPath}}) | {{name}} | {{sources}} |";
 my $readmeGadgetSourcePattern = "<{{url}}> ({{title}})";
 
@@ -23,7 +25,7 @@ my $gadgetsData = LoadFile($gadgetsFile);
 my $episodesData = LoadFile($episodesFile);
 my %episodes = ();
 foreach $episode (@$episodesData) {
-	$episodes{$episodes->{"episode"}} = $episode;
+	$episodes{$episode->{"episode"}} = $episode;
 }
 
 my $readmeTemplate = path($readmeTemplateFile)->slurp({binmode => ":encoding(UTF-8)"});
@@ -38,6 +40,9 @@ foreach $gadget (@$gadgetsData) {
 	my $gadgetId = sprintf($gadgetIdPattern, $gadgetMajor, $gadgetMinor);
 	my $htmlOutputPath = sprintf($htmlOutputPathPattern, $gadgetId);
 
+	###
+	# Generate lines for README.md
+	###
 	my @readmeGadgetSourcesList = ();
 	foreach $source ($gadget->{'sources'}->@*) {
 		my $readmeGadgetSource = $readmeGadgetSourcePattern =~ s/{{url}}/$source->{'url'}/gr;
@@ -51,6 +56,42 @@ foreach $gadget (@$gadgetsData) {
 	$readmeGadget =~ s/{{name}}/$gadget->{'name'}/g;
 	$readmeGadget =~ s/{{sources}}/$readmeGadgetSources/g;
 	push(@readmeGadgetsList, $readmeGadget);
+
+	###
+	# Generate HTML file
+	###
+	if (!path($htmlOutputPath)->exists) {
+		my @linksList = ();
+		foreach $linkData ($gadget->{'links'}->@*) {
+			my $link = $linkPattern =~ s/{{url}}/$linkData->{'url'}/gr;
+			$link =~ s/{{title}}/$linkData->{'title'}/g;
+			push(@linksList, $link);
+		}
+		my $links = join "\n", @linksList;
+
+		my @imageSourcesList = ();
+		foreach $imageSourceData ($gadget->{'sources'}->@*) {
+			my $imageSource = $imageSourcePattern =~ s/{{url}}/$imageSourceData->{'url'}/gr;
+			$imageSource =~ s/{{title}}/$imageSourceData->{'title'}/g;
+			push(@imageSourcesList, $imageSource);
+		}
+		my $imageSources = join "\n", @imageSourcesList;
+
+		my $images = 'TODOs';
+
+		my $episode = $episodes{$gadget->{'episode'}};
+		my $html = $htmlTemplate =~ s/{{gadget}}/$gadget->{'gadget'}/gr;
+		$html =~ s/{{episode}}/$gadget->{'episode'}/g;
+		$html =~ s/{{gadgetName}}/$gadget->{'name'}/g;
+		$html =~ s/{{episodeDate}}/$gadget->{'date'}/g;
+		$html =~ s/{{episodeTitle}}/$episode->{'title'}/g;
+		$html =~ s/{{episodeLink}}/$episode->{'link'}/g;
+		$html =~ s/{{imageSources}}/$imageSources/g;
+		$html =~ s/{{links}}/$links/g;
+		$html =~ s/{{images}}/$images/g;
+
+		path($htmlOutputPath)->append($html);
+	}
 }
 
 my $readmeGadgets = join "\n", @readmeGadgetsList;
